417cb32686591e07e63baad5987a709b8d286681
ayush572 <ayushdagaeinstein@gmail.com>
2023-07-20 01:23:26 +0530

    tech skills section template made

--- src/constants/index.js
+++ src/constants/index.js
@@ -61,60 +61,60 @@
     },
   ];
   
-  // const technologies = [
-  //   {
-  //     name: "HTML 5",
-  //     icon: html,
-  //   },
-  //   {
-  //     name: "CSS 3",
-  //     icon: css,
-  //   },
-  //   {
-  //     name: "JavaScript",
-  //     icon: javascript,
-  //   },
-  //   {
-  //     name: "TypeScript",
-  //     icon: typescript,
-  //   },
-  //   {
-  //     name: "React JS",
-  //     icon: reactjs,
-  //   },
-  //   {
-  //     name: "Redux Toolkit",
-  //     icon: redux,
-  //   },
-  //   {
-  //     name: "Tailwind CSS",
-  //     icon: tailwind,
-  //   },
-  //   {
-  //     name: "Node JS",
-  //     icon: nodejs,
-  //   },
-  //   {
-  //     name: "MongoDB",
-  //     icon: mongodb,
-  //   },
-  //   {
-  //     name: "Three JS",
-  //     icon: threejs,
-  //   },
-  //   {
-  //     name: "git",
-  //     icon: git,
-  //   },
-  //   {
-  //     name: "figma",
-  //     icon: figma,
-  //   },
-  //   {
-  //     name: "docker",
-  //     icon: docker,
-  //   },
-  // ];
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: 'html.png',
+    },
+    {
+      name: "CSS 3",
+      icon: "css.png",
+    },
+    {
+      name: "JavaScript",
+      icon: "javascript.png",
+    },
+    {
+      name: "TypeScript",
+      icon: "typescript.png",
+    },
+    {
+      name: "React JS",
+      icon: "reactjs.png",
+    },
+    {
+      name: "Redux Toolkit",
+      icon: "redux.png",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: "tailwind.png",
+    },
+    {
+      name: "Node JS",
+      icon: "nodejs.png",
+    },
+    {
+      name: "MongoDB",
+      icon: "mongodb.png",
+    },
+    {
+      name: "Three JS",
+      icon: "threejs.png",
+    },
+    {
+      name: "git",
+      icon: "git.png",
+    },
+    {
+      name: "figma",
+      icon: "figma.png",
+    },
+    {
+      name: "docker",
+      icon: "docker.png",
+    },
+  ];
   
   const experiences = [
     {
@@ -268,4 +268,4 @@
     },
   ];
   
-  export {services, experiences, testimonials, projects, navLinks };
+  export {services, experiences, testimonials, projects, navLinks, technologies };
